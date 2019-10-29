4d4cb8351072ca32ffd7cd6ee899f4906e27a8cf
Kevin <you@example.com>
2019-10-29 14:19:45 -0500

    Added Scraper File and Model File Names

--- lib/CLI/cli.rb
+++ lib/CLI/cli.rb
@@ -4,19 +4,9 @@ def run
 end
 
 def welcome
-    puts "Hello! Welcome to this app. Also, you're welcome. And, FU."
+    puts "Welcome to Haunt. Let's find something to scare you."
 end
 
 # def get_user_input
 #     input = gets.chomp
 # end
-
-def find_posts(topic)
-    topic.posts
-  end
-
-  def show_posts(topics)
-    posts.each do |topic|
-      #how could we output each post name here?
-     end
-  end
