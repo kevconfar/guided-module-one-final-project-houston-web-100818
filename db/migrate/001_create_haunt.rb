549731e4616bb11980217fa59ee66ff7bfebe3c6
Heidi Gally <heidigally@gmail.com>
2019-10-30 11:16:20 -0500

    added tty-prompt gem, added to haunt table

--- db/migrate/001_create_haunt.rb
+++ db/migrate/001_create_haunt.rb
@@ -2,8 +2,10 @@ class CreateHaunt < ActiveRecord::Migration[5.0]
   def change
     create_table :haunts do |t|
       t.string :name
+      t.string :category
       t.string :location
       t.string :url
+
     end
   end
 end
