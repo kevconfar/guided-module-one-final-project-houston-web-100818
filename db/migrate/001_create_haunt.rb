1bbee40e7e61a062a9144492a63fbd20b556cad3
Heidi Gally <heidigally@gmail.com>
2019-10-30 12:48:29 -0500

    all updated migrate files and CLI code

--- db/migrate/001_create_haunt.rb
+++ db/migrate/001_create_haunt.rb
@@ -2,6 +2,9 @@ class CreateHaunt < ActiveRecord::Migration[5.0]
   def change
     create_table :haunts do |t|
       t.string :name
+      t.string :category
+      t.string :location
+      t.string :url
     end
   end
 end
