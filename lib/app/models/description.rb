5110738728be672738e12b9b9778fcf4f8c9edc6
Heidi Gally <heidigally@gmail.com>
2019-11-04 13:10:01 -0600

    moore changes to migrate and models

--- lib/app/models/description.rb
+++ lib/app/models/description.rb
@@ -6,3 +6,7 @@ class Description < ActiveRecord::Base
     has_many :haunts, through: :haunt_descriptions
 
 end 
+
+visual = [orbs, shadow, figure, lights, items_moving, apparition]
+auditory = [disembodied, voice, scream, growl, footsteps, knocking, cries, giggling, laughing, moaning, unexplained_sounds]
+physical = [thrown_item, cold_spot, poltergeist, touch, scratch, possess]
