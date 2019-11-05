c0e41e193ac40682d9ac4f32030ab0313a87358f
Kevin <you@example.com>
2019-11-05 11:59:11 -0600

    Completed Scraper for HauntedRooms

--- lib/CLI/cli.rb
+++ lib/CLI/cli.rb
@@ -103,21 +103,22 @@ def response_choices
   end
   
 
-def run
-    welcome
-    input = gets_user_input
-    find_haunt(input)
-end
-
-def welcome
-    puts "Welcome to Haunt. Let's find something to scare you."
-end
-
-# def get_user_input
-#     input = gets.chomp
+# def run
+#     welcome
+#     input = gets_user_input
+#     find_haunt(input)
 # end
-def gets_user_input
-    puts "We can help you find haunted hotels near you!"
-    puts "Enter a location to get started:"
-    location = gets.chomp
-end
+
+# def welcome
+#     puts "Welcome to Haunt. Let's find something to scare you."
+# end
+
+# # def get_user_input
+# #     input = gets.chomp
+# # end
+# def gets_user_input
+#     puts "We can help you find haunted hotels near you!"
+#     puts "Enter a location to get started:"
+#     location = gets.chomp
+# end
+
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -119,7 +119,7 @@ require 'open-uri'
 #     descriptions += [last.join(".")]
 # end
 
-# puts test_descriptions[-1]
+# puts test_descriptions
 
 
 #------------------------------------------------------------------------------------------
@@ -142,108 +142,163 @@ require 'open-uri'
 # haunt_hash = {}
 
 
-def hrefs
-    html = open("https://www.hauntedrooms.com/haunted-places")
-    doc = Nokogiri::HTML(html)
+# def hrefs
+#     html = open("https://www.hauntedrooms.com/haunted-places")
+#     doc = Nokogiri::HTML(html)
 
-    return doc.css('div.entry-content li a').map { |link| link['href'] }
-end
+#     return doc.css('div.entry-content li a').map { |link| link['href'] }
+# end
 
-def href_arr # will insert state_href as argument. If done on a loop, perhaps use shift to delete index 0 each time.
-    arr = []
-    hrefs.each do |state|
-        arr << "https://www.hauntedrooms.com#{state}"
-    end
-    
-end
+# def href_arr # will insert state_href as argument. If done on a loop, perhaps use shift to delete index 0 each time.
+#     arr = []
+#     hrefs.each do |state|
+#         str = "https://www.hauntedrooms.com"
+#         str += "#{state}"
+#         arr << str
+#     end
+#     arr
+# end
+
+# def state_name # takes in state href as an argument and pulls up that states name
+#   arr = []
+#   href_arr.each do |state_href|
+
+#       tester = "#{state_href}"
+
+#       html = open("#{tester}")
+#       doc = Nokogiri::HTML(html)
+
+#       state = doc.css(".entry-title").text.split(" ").last
+#       arr << state
+#     end
+#     arr
+# end
+
+# def haunt_info
+#     haunt_names = []
+#     href_arr.each do |state_href|
+        
+#         href = state_href
+
+#         html = open("#{href}")
+#         doc = Nokogiri::HTML(html)
+
+#         places = doc.css(".section-title-main")
+
+#         places.each do |index|
+#             haunt = index.text[4..-1]
+#             arr = [haunt.split(", ")]
+#             haunt_names += arr
+#         end
+#     end
+#     haunt_names
+# end
+
+# def descriptions
+#     arr = []
+#     h_description = []
+
+#     href_arr.each do |state_href|
+
+#       href = "#{state_href}"
+
+#       html = open("#{href}")
+#       doc = Nokogiri::HTML(html)
+
+#       unparsed_info = doc.css(".page-inner p")
+
+#       paragraph = ""
+#       x = unparsed_info[3..-1]
+#       x.each do |index|
+#           description = index.text
+#           if !description.empty?
+#               paragraph << description
+#           else
+#               paragraph << "*"
+#           end
+#       end
+
+#       h_description = paragraph.split("*")
+#       last = h_description.pop.split(".")
+#       h_description += [last[0..-2].join(".")]
+#       arr << [h_description]
+#     end
+#     arr
+
+# end
+
+# puts descriptions 
 
 
-def state_name(state_href) # takes in state href as an argument and pulls up that states name
-    html2 = open("#{state_href}")
-    doc2 = Nokogiri::HTML(html2)
-
-    state = doc2.css(".entry-title").text.split(" ").last
-end
-
-def haunt_info(state_href)
-
-    html = open("#{state_href}")
-    doc = Nokogiri::HTML(html)
-
-    places = doc.css(".section-title-main")
-
-    haunt_names = []
-    places.each do |index|
-        haunt = index.text[4..-1]
-        arr = [haunt.split(", ")]
-        haunt_names += arr
-    end
-    haunt_names
-end
+#     arr = []
 
 
-def descriptions(state_href)
+#     href_arr.each do |state_href|
+        
+#         href = state_href
 
-    html = open("\"#{state_href}\"")
-    doc = Nokogiri::HTML(html)
+#         html = open("#{state_href}")
+#         doc = Nokogiri::HTML(html)
 
-    unparsed_info = doc.css(".page-inner p")
+#         unparsed_info = doc.css(".page-inner p")
 
-    paragraph = ""
-    x = unparsed_info[3..-1]
-    x.each do |index|
-        description = index.text
-        if !description.empty?
-            paragraph << description
-        else
-            paragraph << "*"
-        end
-    end
+#         paragraph = ""
+#         x = unparsed_info[3..-1]
+#         x.each do |index|
+#             description = index.text
+#             if !description.empty?
+#                 paragraph << description
+#             else
+#                 paragraph << "*"
+#             end
+#         end
 
-    descriptions = paragraph.split("*")
-    last = descriptions[-1].split(".")
-    last.pop
-    descriptions.pop
-    descriptions += [last.join(".")]
-end
+#         descriptions = paragraph.split("*")
+#         last = descriptions.pop.split(".")
+#         descriptions += last[0..-2]
+#         arr << descriptions
+#     end
+#     arr
+# end
 
-def print_descriptions
-    arr = []
-    href_arr.each do |state_href|
-        tester = descriptions("#{state_href}")
-        arr += tester
-    end
-    arr
-end
+
+
+
+# def print_descriptions
+#     arr = []
+#     href_arr.each do |state_href|
+#         tester = descriptions("#{state_href}")
+#         arr += tester
+#     end
+#     arr
+# end
 
 # puts print_descriptions
         
 
 
-def hash_maker
+# def hash_maker
 
-    href_arr.each do |state_href|
-        state = state_name("#{state_href}")
-        info = haunt_info("#{state_href}")
-        haunt_descriptions = descriptions("#{state_href}")
+#     haunt_hash = {}
 
-        info.each do |haunt| 
-            haunt_descriptions.each do |haunt_description|
-                if !haunt_hash[state]
-                    haunt_hash[state] = [{ :name => info[0], :location => info[1], :description => haunt_description }]
-                else
-                    haunt_hash[state] << { :name => info[0], :location => info[1], :description => haunt_description }
-                end
-            end
-        end
+#     state_name.each do |state|
+#         haunt_info.each do |haunt| 
+#             descriptions.each do |state_haunts|
+#                 state_haunts.each do |index|
+#                 if !haunt_hash[state]
+#                     haunt_hash[state] = [{ :name => haunt[0], :location => haunt[1], :description => index }]
+#                 else
+#                     haunt_hash[state] << { :name => haunt[0], :location => haunt[1], :description => index }
+#                 end
+#             end
+#         end
+#     end
 
-    end
-    haunt_hash
-end
-
+#     haunt_hash
+# end
 
 
-
+# puts hash_maker
 
 
 # puts state_name(href_arr)
@@ -343,3 +398,421 @@ end
 #         hash[state] << { :name => index[0], :location => index[1], :description => descriptions[haunt_names.index(place)] }
 #     end
 # end
+
+
+# def hrefs
+#     html = open("https://www.hauntedrooms.com/haunted-places")
+#     doc = Nokogiri::HTML(html)
+
+#     return doc.css('div.entry-content li a').map { |link| link['href'] }
+# end
+
+
+# def href_arr # will insert state_href as argument. If done on a loop, perhaps use shift to delete index 0 each time.
+#     arr = []
+#     hrefs.each do |state|
+#         str = "https://www.hauntedrooms.com"
+#         str += "#{state}"
+#         arr << str
+#     end
+#     arr
+# end
+
+
+# def descriptions(state_href)
+
+#   href = "#{state_href}"
+
+#   html = open("#{href}")
+#   doc = Nokogiri::HTML(html)
+
+#   unparsed_info = doc.css(".page-inner p")
+
+#   paragraph = ""
+#   x = unparsed_info[3..-1]
+#   x.each do |index|
+#       description = index.text
+#       if !description.empty?
+#           paragraph << description
+#       else
+#           paragraph << "*"
+#       end
+#   end
+
+#   h_description = paragraph.split("*")
+#   last = h_description.pop.split(".")
+#   h_description += [last[0..-2].join(".")]
+#   h_description
+# end
+
+# def haunt_info(state_href)
+
+#   haunt_names = []
+
+#   href = "#{state_href}"
+
+#   html = open("#{href}")
+#   doc = Nokogiri::HTML(html)
+
+#   places = doc.css(".section-title-main")
+
+#   places.each do |index|
+#       haunt = index.text[4..-1]
+#       arr = [haunt.split(", ")]
+#       haunt_names << arr
+#   end
+
+#   haunt_names
+# end
+
+# def state_name(state_href)
+
+#   href = "#{state_href}"
+#   arr = []
+
+
+#   html = open("#{href}")
+#   doc = Nokogiri::HTML(html)
+
+#   state = doc.css(".entry-title").text.split(" ").last
+#   arr << state
+# end
+
+
+# def description_test
+#   tester = []
+#   href_arr.each do |state_href|
+#     href = "#{state_href}"
+#     example = descriptions("#{href}")
+#     tester << example
+#   end
+#   tester
+# end
+
+# puts description_test
+
+# def hash_maker
+#   haunt_hash = {}
+
+#   href_arr.each do |state_href|
+#     haunt_descriptions = descriptions("#{state_href}")
+#     info = haunt_info("#{state_href}")
+#     state = state_name("#{state_href}")
+#     haunt_descriptions.each do |description|
+#         x = 0
+#         y = 1
+#         info.each do |name_loc|
+#             if !haunt_hash[state]
+#             haunt_hash[state] = [{ :name => name_loc[x], :location => name_loc[y], :description => description }]
+#             else
+#             haunt_hash[state] << { :name => name_loc[x], :location => name_loc[y], :description => description }
+#             end
+#             x += 2
+#             y += 2
+#         end
+#     end
+#   end
+#   haunt_hash
+# end
+
+# puts hash_maker
+
+
+
+
+
+
+
+
+
+
+
+# def hash_tester
+
+#   haunt_hash = {}
+
+#   href_arr.each do |state_href|
+#     address = "#{state_href}"
+
+
+#     state = state_name("#{address}")
+#     describe = descriptions("#{address}")
+#     information = haunt_info("#{address}")
+#     information.each do |name_location|
+#       name_location.each do |name, location|
+#         describe.each do |haunt_desc|
+#           if !haunt_hash[state]
+#             haunt_hash[state] = [{ :name => name, :location => location, :description => haunt_desc }]
+#           else
+#             haunt_hash[state] << { :name => name, :location => location, :description => haunt_desc }
+#           end
+#         end
+#       end
+#     end
+#   end
+#   haunt_hash
+# end
+
+# def descriptions(state_href)
+
+#   href = "#{state_href}"
+
+#   html = open("#{href}")
+#   doc = Nokogiri::HTML(html)
+
+#   unparsed_info = doc.css(".page-inner p")
+
+#   paragraph = ""
+#   x = unparsed_info[3..-1]
+#   x.each do |index|
+#       description = index.text
+#       if !description.empty?
+#           paragraph << description
+#       else
+#           paragraph << "*"
+#       end
+#   end
+
+#   h_description = paragraph.split("*")
+#   last = h_description.pop.split(".")
+#   h_description += [last[0..-2].join(".")]
+#   h_description
+# end
+
+# def haunt_descriptions
+#   tester = []
+#   href_arr.each do |state_href|
+#     href = "#{state_href}"
+#     example = descriptions("#{href}")
+#     tester << example
+#   end
+#   tester
+# end
+
+#----------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------
+
+
+# def haunt_info(state_href)
+#   haunt_names = []
+
+#     href = "#{state_href}"
+
+#     html = open("#{href}")
+#     doc = Nokogiri::HTML(html)
+
+#     places = doc.css(".section-title-main")
+
+#     places.each do |index|
+#       arr = []
+#       haunt = index.text[3..-1]
+#       haunt_names << haunt.split(", ")
+#     end
+ 
+#   haunt_names[0..-2]
+# end
+
+
+# def state_name(state_href)
+
+#   href = "#{state_href}"
+
+#   html = open("#{href}")
+#   doc = Nokogiri::HTML(html)
+
+#   state = doc.css(".entry-title").text.split(" ").last
+# end
+
+# def descriptions(state_href)
+
+#   href = "#{state_href}"
+
+#   html = open("#{href}")
+#   doc = Nokogiri::HTML(html)
+
+#   unparsed_info = doc.css(".page-inner p")
+
+#   paragraph = ""
+#   x = unparsed_info[3..-1]
+#   x.each do |index|
+#       description = index.text
+#       if !description.empty?
+#           paragraph << description
+#       else
+#           paragraph << "*"
+#       end
+#   end
+
+#   h_description = paragraph.split("*")
+#   last = h_description.pop.split(".")
+#   h_description += [last[0..-2].join(".")]
+#   h_description
+# end
+
+
+# def hash_tester
+
+#   haunt_hash = {}
+
+#   href_arr.each do |state_href|
+#     address = "#{state_href}"
+
+#     state = state_name("#{address}")
+#     describe = descriptions("#{address}")
+#     information = haunt_info("#{address}")
+#     information.each do |name_location|
+#       name_location.each do |name, location|
+#         describe.each do |haunt_desc|
+#           if !haunt_hash[state]
+#             haunt_hash[state] = [{ :name => name, :location => location, :description => haunt_desc }]
+#           else
+#             haunt_hash[state] << { :name => name, :location => location, :description => haunt_desc }
+#           end
+#         end
+#       end
+#     end
+#   end
+#   haunt_hash
+# end
+
+# puts hash_tester
+
+
+
+
+#----------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------
+
+
+def hrefs
+    html = open("https://www.hauntedrooms.com/haunted-places")
+    doc = Nokogiri::HTML(html)
+
+    return doc.css('div.entry-content li a').map { |link| link['href'] }
+end
+
+def href_arr # will insert state_href as argument. If done on a loop, perhaps use shift to delete index 0 each time.
+    arr = []
+    hrefs.each do |state|
+        str = "https://www.hauntedrooms.com"
+        str += "#{state}"
+        arr << str
+    end
+    arr
+end
+
+def state_name(state_href)
+
+  href = "#{state_href}"
+
+  html = open("#{href}")
+  doc = Nokogiri::HTML(html)
+
+  state = doc.css(".entry-title").text.split(" ").last
+end
+
+def haunt_info(state_href)
+  haunt_names = []
+
+    href = "#{state_href}"
+
+    html = open("#{href}")
+    doc = Nokogiri::HTML(html)
+
+    places = doc.css(".section-title-main")
+
+    places.each do |index|
+      arr = []
+      haunt = index.text[3..-1]
+      haunt_names << haunt.split(", ")
+    end
+  # end
+  haunt_names[0..-2]
+end
+
+def descriptions(state_href)
+
+  href = "#{state_href}"
+
+  html = open("#{href}")
+  doc = Nokogiri::HTML(html)
+
+  unparsed_info = doc.css(".page-inner p")
+
+  paragraph = ""
+  x = unparsed_info[3..-1]
+  x.each do |index|
+      description = index.text
+      if !description.empty?
+          paragraph << description
+      else
+          paragraph << "*"
+      end
+  end
+
+  h_description = paragraph.split("*")
+  last = h_description.pop.split(".")
+  h_description += [last[0..-2].join(".")]
+  h_description
+end
+
+def haunt_hasher
+
+  haunt_hash = {}
+  href_arr.each do |state_href|
+
+    haunt_hash = {}
+    href = "#{state_href}"
+
+    info = haunt_info(href)
+    state = state_name(href)
+    about = descriptions(href)
+    
+    info.each do |haunt|
+      about.each do |describe|
+        if haunt.length == 3
+          if !haunt_hash[state]
+              haunt_hash[state] = [{ :name => haunt[0], :city => haunt[1], :state => haunt[2], :description => describe }]
+            # end
+          else
+            # haunt.each do |name, city, state_name|
+              haunt_hash[state] << { :name => haunt[0], :city => haunt[1], :state => haunt[2], :description => describe }
+            # end
+          end
+        else
+          if !haunt_hash[state]
+            # haunt.each do |name, city|
+              haunt_hash[state] = [{ :name => haunt[0], :city => haunt[1], :state => state, :description => describe }]
+            # end
+          else
+            # haunt.each do |name, city|
+              haunt_hash[state] << { :name => haunt[0], :city => haunt[1], :state => state, :description => describe }
+            # end
+          end
+        end
+      end
+    end
+  end
+  haunt_hash
+end
+
+puts haunt_hasher
+
+  
+
+
+
+
+
+
+
+
+
